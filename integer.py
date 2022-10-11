20689c448f5112fbc9cad2db5c48e6c8bf0ee68b
Krostofer <christopher.di-bert@stu.ocsb.ca>
2022-10-11 21:59:34 +0000

    Added comments.

--- integer.py
+++ integer.py
@@ -6,14 +6,18 @@
 
 
 def main():
+    # Gets the user's integer
     user_integer = int(input("Enter an integer: "))
 
+    # Code executed if the integer is greater than zero.
     if user_integer > 0:
         print(f"{user_integer} is positive.")
 
+    # Code executed if the integer is less than zero.
     elif user_integer < 0:
         print(f"{user_integer} is negative.")
 
+    # Code executed if the integer is zero.
     else:
         print("Your integer is not negative or positive (0).")
 
